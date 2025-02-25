734ddc8794983bdf24593db7c2c1bc926d4fae51
zimka1 <zimkail2015@mail.ru>
2025-02-25 16:38:35 +0100

    Hexadecimal conversion implemented as an external procedure

--- hello.asm
+++ hello.asm
@@ -4,6 +4,8 @@ INCLUDE macros.inc
 .model small
 
 .code
+EXTRN print_hex:near
+
 START:
     mov ax, DGROUP
     mov ds, ax
@@ -124,7 +126,7 @@ continue_without_space:
 
     pop ax
 
-    call convert_to_hex
+    call print_hex
 
     inc si
 
@@ -137,42 +139,11 @@ print_offset:
     print_line_feed
     push ax
     mov ax, [file_offset]
-    call convert_to_hex
+    call print_hex
     pop ax
     inc si
     loop read_loop
 
-convert_to_hex:
-    push ax
-    push bx
-    push cx
-
-    mov ah, al     ; Save byte in AH
-    shr al, 4      ; Keep only the high nibble
-    call convert_num
-
-    mov al, ah     ; Restore original byte
-    and al, 0Fh    ; Keep only the low nibble (0xAB → 0x0B)
-    call convert_num
-    
-    pop cx
-    pop bx
-    pop ax
-    ret
-
-convert_num:
-    push ax   ; Save AL
-    and al, 0Fh    ; Keep only the lowest 4 bits
-    add al, '0'    ; Convert 0–9 to '0'–'9'
-    cmp al, '9'    
-    jbe print_char 
-    add al, 7h     ; Convert A–F (A=41h, B=42h, ...)
-
-print_char:
-    mov ah, 0Eh    ; BIOS function for character output
-    int 10h
-    pop ax   ; Restore AL
-    ret
 
 close_file:
     ; Close the file
